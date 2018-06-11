5b8f32a194e08531305a4365af3de761f6335c42
Bob Ross <ministash14@gmail.com>
2018-06-10 22:21:56 -0700

    Cleaned things up, the gifs now play and pause

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,107 +1,85 @@
-$(document).ready(function(){
+    var topics = ["batman", "superman", "green lantern", "wonder woman", "the flash", "aquaman", "bruce wayne", "tony stark", "ironman", "hulk"];
+    var gifUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+        gif + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var gif = "";
 
 
-var topics = ["batman", "superman", "green lantern", "wonder woman", "the flash", "aquaman", "bruce wayne", "tony stark", "ironman", "hulk"];
-var gifUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-gif + "&api_key=dc6zaTOxFJmzC&limit=10";
-var gif = "";
-var running = false;
+    console.log(gifUrl);
 
+    //This is my button maker. It also assigns the name of the button as a id to that buttons div.
+    function buttonMaker() {
+        for (i = 0; i < topics.length; i++) {
+            $("#buttons").append(
+                $("<div/>")
+                    .attr('id', topics[i])
+                    .addClass('x topicButtons')
+                    .text(topics[i])
+            );
 
-console.log(gifUrl);
+        }
 
-//This is my button maker. It also assigns the name of the button as a id to that buttons div.
-function buttonMaker(){
-    for(i = 0; i < topics.length; i++){
-        $("#buttons").append(
-            $("<div/>")
-            .attr('id', topics[i])
-            .addClass('x topicButtons')
-            .text(topics[i])
-        );
-                
     }
 
-}
-
-buttonMaker();
+    buttonMaker();
 
 
-//This is the function that controls my onclick event for my created buttons
-$(".topicButtons").on("click", function(){
-    $("#imageArea").html("");
-        gif = this.id;
-        gifUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-        gif + "&api_key=dc6zaTOxFJmzC&limit=10";
+    //This is the function that controls my onclick event for my created buttons
+    $(".topicButtons").on("click", function () {
+        $("#imageArea").html("");
+        gif = $(this).attr("id");
+        gifUrl = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
         console.log(gif);
         console.log(gifUrl);
 
-      
+//this is also where I store the API data for the gifs displayed in imageArea. I call on my imgCreator to actually create the gifs.
+        $.ajax({
+            url: gifUrl,
+            method: "GET"
+        }).then(function (response) {
 
-      $.ajax({
-        url: gifUrl,
-        method: "GET"
-      }).then(function(response){
+            var groupGifUrl = response.data;
 
-
-        
-     var groupGifUrl = response.data;
-
-     
-     for(i = 0; i < groupGifUrl.length; i++){
-         
-         var imageTag = $("<img>");
-
-            imageTag.attr(
-                {
-                    "width": "200",
-                    "height": "200",
-                    "class": "img",
-                    "src": groupGifUrl[i].images.original_still.url,
-                    "data-still": groupGifUrl[i].images.original_still.url,
-                    "data-animate": groupGifUrl[i].images.original.url,
-                    "data-state" : "still"
-
-                }
-            );
-
-            $("#imageArea").append(imageTag);
-        }
+            imgCreator(groupGifUrl);
             
-        var state = $(".img").attr("data-state");
-
-            $(".img").on("click", function(){
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                  } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-            } 
         });
-            
-            
-
         
-             
+    })
+
+    
+//This is where the gifs are created and given attributes. Also this is where I store my onclick function for pausing or starting them.
+function imgCreator(groupGifUrl){
+    for (i = 0; i < groupGifUrl.length; i++) {
+        $("#imageArea").append(
+                $("<img>")
+                    .attr({
+                        "width": "200",
+                        "height": "200",
+                        "class": "img",
+                        "src": groupGifUrl[i].images.original_still.url,
+                        "data-still": groupGifUrl[i].images.original_still.url,
+                        "data-animate": groupGifUrl[i].images.original.url,
+                        "data-state": "still"
+
+                    })
+            );
+        
+    }
+    
+    $(".img").on("click", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            console.log(this);
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
             
-            // console.log(imageTag, "src");
-
-        
-        
-
-      })
- 
+        }
     });
+}
 
 
 
 
 
-
-
-
-
-
-
-    });
